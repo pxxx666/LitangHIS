43bb9807867e945f50fbfe3548f039da4997693c
PX <1209304680@qq.com>
2024-03-31 14:27:27 +0800

    完成登陆界面初始化设计

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,11 +1,153 @@
 <script setup>
+import { ref } from 'vue'
+//控制注册与登录表单的显示， 默认显示注册
+const isRegister = ref(false)
+const registerData = ref({
+  username: '',
+  password: '',
+  rePassword: ''
+})
+//自定义确认密码的校验函数
+const rePasswordValid = (rule, value, callback) => {
+  if (value == null || value === '') {
+    return callback(new Error('请再次确认密码'))
+  }
+  if (registerData.value.password !== value) {
+    return callback(new Error('两次输入密码不一致'))
+  }
+}
+const registerDataRules = ref({
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 3, max: 10, message: '用户名的长度必须为3~10位', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    { min: 6, max: 20, message: '密码长度必须为6~20位', trigger: 'blur' }
+  ],
+  rePassword: [
+    { validator: rePasswordValid, trigger: 'blur' }
+  ]
+})
+const clearRegisterData = () => {
+  registerData.value = {
+    username: '',
+    password: '',
+    rePassword: ''
+  }
+}
+
 
 </script>
 
 <template>
+  <img src="@/assets/images/HISTitle.png" alt="">
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
 
+      <!-- 注册表单 -->
+      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister" :model="registerData"
+               :rules="registerDataRules">
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input :prefix-icon="User" placeholder="请输入用户名" v-model="registerData.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input :prefix-icon="Lock" type="password" placeholder="请输入密码"
+                    v-model="registerData.password"></el-input>
+        </el-form-item>
+        <el-form-item prop="rePassword">
+          <el-input :prefix-icon="Lock" type="password" placeholder="请输入再次密码"
+                    v-model="registerData.rePassword"></el-input>
+        </el-form-item>
+        <!-- 注册按钮 -->
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>
+            注册
+          </el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = false;clearRegisterData()">
+            ← 返回
+          </el-link>
+        </el-form-item>
+      </el-form>
+      <!-- 登录表单 -->
+      <el-form ref="form" size="large" autocomplete="off" v-else :model="registerData" :rules="registerDataRules">
+        <el-form-item>
+          <!-- 标题 -->
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input name="username" :prefix-icon="User" placeholder="请输入用户名"
+                    v-model="registerData.username"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input name="password" :prefix-icon="Lock" type="password" placeholder="请输入密码"
+                    v-model="registerData.password"></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <!-- 登录按钮 -->
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>登录</el-button>
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true;clearRegisterData()">
+            注册 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+/* 样式 */
+img{
+  position: absolute;
+  z-index: 100;
+  width: 600px;
+  right: 75px;
+  top: 30px;
+}
+.login-page {
+  height: 100vh;
+  background-color: #fff;
 
+  .bg {
+    background:
+    url('@/assets/images/Login.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+
+
+    .title {
+      margin: 0 auto;
+    }
+
+    .button {
+      width: 100%;
+    }
+
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
 </style>
